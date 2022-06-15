169d062e29409f4fbe554d52da723fd8a7d0c089
Lena <elena.pokhvalennaya@gmail.com>
2022-06-15 22:41:31 +0300

    update task

--- my-first-app/src/components/Title.js
+++ my-first-app/src/components/Title.js
@@ -1,7 +1,5 @@
 const Title = ({ name, type = "normal" }) => {
-  const fontWeight = type === "bold" ? "bold" : "normal";
-
-  return <h1 style={{ fontWeight }}>It's {name}'s first react app</h1>;
+  return <h1 style={{ fontWeight: type }}>It's {name}'s first react app</h1>;
 };
 
 export default Title;
